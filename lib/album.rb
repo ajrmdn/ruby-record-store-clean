51ba6c1e3da901dc1c7171d77588fe5903e9a102
Student <example@example.com>
2019-07-29 11:43:29 -0700

    clean

--- lib/album.rb
+++ lib/album.rb
@@ -8,7 +8,7 @@ class Album
   end
 
   def self.all
-    returned_albums = DB.exec("SELECT * FROM albums;")
+    returned_albums = DB.exec("SELECT * FROM albums ORDER BY name;")
     albums = []
     returned_albums.each() do |album|
       name = album.fetch("name")
